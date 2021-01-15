7d1bd4ee85b1e91573bbed2854862d99e40c7943
djvangerwen <dennisvangerwen@medishielddelft.com>
2021-01-15 16:20:31 +0100

    rename background_color to theme_color

--- example/settings.py
+++ example/settings.py
@@ -1,12 +1,12 @@
 FILENAME = 'cv.html'
 JSON_FILES = ['cv.json', 'technologies.json']
 
-TEMPLATE_FOLDERS = ['templates']  # relative to "source" folder
+TEMPLATE_FOLDERS = ['templates']  # relative to "source"
 PAGES = ['onepage.html', 'technologies.html', 'custom_publications.html']
 STYLES = ['style_pages.css', 'style_zero.css']
 
-BACKGROUND_COLOR = '#438496'
-FONT_COLOR = 'white'
+THEME_COLOR = '#438496'
+THEME_FONT_COLOR = 'white'
 
 SECTIONS = dict(education=dict(priority=2),
                 languages=dict(level=4),
--- pcv/defaults/settings.py
+++ pcv/defaults/settings.py
@@ -5,8 +5,8 @@ TEMPLATE_FOLDERS = []  # relative to the "source" folder
 PAGES = ['onepage.html', 'technologies.html', 'publications.html']
 STYLES = ['style_pages.css', 'style_zero.css']
 
-BACKGROUND_COLOR = '#438496'
-FONT_COLOR = 'white'
+THEME_COLOR = '#438496'
+THEME_FONT_COLOR = 'white'
 
 SECTIONS = dict(technologies=dict(level=2, priority=2, exclude=[]),
                 skills=dict(level=3, priority=1, source='technologies'))
